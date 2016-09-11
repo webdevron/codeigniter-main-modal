4d925b7ec4e33d65a75ad8ebaebdd0ef73ed9274
M. B. Parvez Ron <mbpron@yahoo.com>
2016-09-11 23:08:37 +0600

    Update Main_modal.php

--- Main_modal.php
+++ Main_modal.php
@@ -3,90 +3,91 @@
 class Main_m extends CI_Model{
 
 	public function check($table, $arr){
-		$query = $this->db->from($table)
-				  ->where($arr)
-				  ->get();
+		$query = $this->db->select()
+						  ->from($table)
+						  ->where($arr)
+						  ->get();
 		return $query->num_rows() > 0 ? TRUE : FALSE;
 	}
 
+
+
 	public function submit($table, $val){
 		$this->db->insert($table, $val);
-		return $this->db->insert_id();
+		return $this->db->insert_id() > 0 ? TRUE : FALSE;
 	}
 
-	public function get_row($table, $key) {
+
+
+	public function get_all($table){
 		$query = $this->db->select()
-				  ->from($table)
-				  ->where($key)
-				  ->get();
-		return $query->row();
+						  ->from($table)
+						  ->get();
+		return $query->result();
 	}
 
 	public function get_many($table, $arr){
 		$query = $this->db->select()
-				  ->from($table)
-				  ->where($arr)
-				  ->get();
+						  ->from($table)
+						  ->where($arr)
+						  ->get();
 		return $query->result();
 	}
 
-	public function get_all($table) {
+	public function get_row($table, $arr){
 		$query = $this->db->select()
-				  ->from($table)
-				  ->get();
-		return $query->result();
-	}
-
-	public function get_like($table, $arr){
-		$query = $this->db->select()
-				  ->from($table)
-				  ->like($arr)
-				  ->get();
-		return $query->result();
+						  ->from($table)
+						  ->where($arr)
+						  ->get();
+		return $query->row();
 	}
 
 
 
-	public function update_by($table, $arr, $key){
-		$this->db->update($table, $arr, $key);
-		return TRUE;
+	public function update_all($table, $arr){
+		return $this->db->update($table, $arr) ? TRUE : FALSE;
+	}
+ 
+	public function update_by($table, $arr, $by){
+		return $this->db->update($table, $arr, $by) ? TRUE : FALSE;
 	}
 
-	public function update_all($table, $arr, $key){
-		$this->db->update($table, $arr, $key);
-		return TRUE;
-	}
 
-	public function count_all($table){
+
+	public function count_rows($table){
 		return $this->db->count_all($table);
 	}
 
 	public function count_by($table, $arr){
 		$query = $this->db->from($table)
-				  ->where($arr)
-				  ->get();
+						  ->where($arr)
+						  ->get();
 		return $query->num_rows();
 	}
 
-
-	// ================= OPERATIONAL FUNCTIONS ================= //
-	public function order($by, $s = FALSE){
-		$t = $s === FALSE ? 'ASC' : 'desc';
-		$this->db->order_by($by,$t);
+	public function _group($arr){
+		$this->db->group_by(array("title", "date"));
 		return $this;
 	}
 
-	public function limit($limit, $offset = 0){
+
+	public function _order($by, $s = FALSE){
+		$t = $s === FALSE ? 'ASC' : 'desc';
+		$this->db->order_by($by, $t);
+		return $this;
+	}
+
+	public function _limit($limit, $offset = 0){
 		$this->db->limit($limit, $offset);
 		return $this;
 	}
 
-	public function join($table, $match){
+	public function _join($table, $match){
 		$this->db->join($table, $match);
 		return $this;
 	}
 
-	public function delete($table, $match){
+	public function _delete($table, $match){
 		$this->db->delete($table, $match);
 		return $this;
 	}
